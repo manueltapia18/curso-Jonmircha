a066de6052b09bbcf6f931c68e26a3b19971928a
Manuel Esteban Tapia <manuel.tapia.9318@hotmail.com>
2023-03-15 20:52:29 -0500

    quedamos por el video 66 el proximo video en ver es el 67 que es traversing

--- dom.js
+++ dom.js
@@ -60,3 +60,58 @@ varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color");
 
 
 $body.style.setProperty("background-color",varDarkColor);
+
+//clases css DOM//
+
+const $card =document.querySelector(".card");
+console.log($card);
+console.log($card.className);
+console.log($card.classList);
+console.log($card.classList.contains("rotate-45"));
+$card.classList.add("rotate-45");
+console.log($card.className);
+console.log($card.classList);
+$card.classList.remove("rotate-45");
+console.log($card.classList.contains("rotate-45"));
+// el metodo toggle si tiene una clase, se la quita, y si la tiene se la agrega ejemplo//
+$card.classList.toggle("rotate-45");
+console.log($card.classList.contains("rotate-45"));
+$card.classList.toggle("rotate-45");
+console.log($card.classList.contains("rotate-45"));
+$card.classList.toggle("rotate-45");
+//el metodo replace reemplaza una clase por otra//
+$card.classList.replace("rotate-45", "rotate-135");
+//para agregar o eliminar varias clases a la vez solo basta con agregar una coma entre ellas ejemplo:
+$card.classList.add("opacity-80","sepia");
+console.log($card.classList.contains("opacity-80","sepia"));
+
+// DOM texto y html//
+
+const $whatIsDom = document.getElementById("que-es")
+
+
+
+let text = `
+    <p>
+    El modelo de objetos del documento(<b><i>DOM- document Object model</i></b>)es un API para documentos HTML y XML.
+    </p>
+    <p>
+    este provee una representacion estructural del documento, permitiendo modificar su contenido y presentacion visual mediante codigo JS.
+    </p>
+    <p>
+    <mark> el DOM no es parte de la espesificacion de javascript, es una API para los navegadores, </mark>
+    </p>
+`;
+
+// el innerText no hace parte del estandar fue creado solo para internet explorer y agrega con todo y etiquetas
+$whatIsDom.innerText = text;
+//tenemos tambien textContent que al igualk que innerText no agrega las etiquetas respeta los espaciados y es mas utilizado ejemplo:
+$whatIsDom.textContent = text;
+//mientras que innerHTML si reenderiza las etiquetas colocadas ejemplo p, br etx etiquetas de html ejmplo:
+$whatIsDom.innerHTML = text;
+
+//la diferencia entre textContent y InnerHTML es que textContent se debe usas cuando se vaya a ingresar solo texto e innerthtml cuando se vaya a ingresar html 
+
+//tambien podemos usar la propiedad outerHTML esta reemplaza de mejor manera lo colocado de html//
+$whatIsDom.outerHTML = text;
+
