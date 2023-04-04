5b9c8d545ab3ef848cd80ed26f019e02afab02d4
Manuel Esteban Tapia <manuel.tapia.9318@hotmail.com>
2023-04-04 00:36:54 -0500

    seguir con el ejercicio

--- ejercicios/ejercicios-DOM/index.js
+++ ejercicios/ejercicios-DOM/index.js
@@ -1,9 +1,34 @@
 import hamburguerMenu from "./menu_hamburguesa.js";
 
 const d = document;
+const reloj = document.getElementById("reloj");
+const date = document.getElementById("date");
+
 
 d.addEventListener("DOMContentLoaded", (e)=>{
   hamburguerMenu(".panel-btn",".panel", ".menu a");
 })
+
+function numero() {
+  let fecha = new date('December 17, 1995 03:24:00');
+  fecha.toLocaleTimeSTring('en-US');
+  date.appendChild(fecha);
+  console.log(date);
+}
+
+reloj.addEventListener("click",numero);
+
+// export default function name(params) {
+//   const reloj = document.getElementById("reloj");
+
+//   let hora = new Date();
+//   hora.toLocaleTimeString();
+//   const Date = document.getElementById("Date");
+//   Date.addEventListener("click", (e)=>{
+  
+//   })
+// }
+
+
 
 
